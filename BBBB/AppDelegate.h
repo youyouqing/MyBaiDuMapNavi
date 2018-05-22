83b87bfb7bd89845c54b8d41fe21b0d1f3cb8a8d
youyouqing <423263279@qq.com>
2018-05-23 00:45:25 +0800

    Initial Commit

---
+++ BBBB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBBB
+//
+//  Created by zhangmin on 2018/5/23.
+//  Copyright © 2018年 zhangmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBBB/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BBBB
+//
+//  Created by zhangmin on 2018/5/23.
+//  Copyright © 2018年 zhangmin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
